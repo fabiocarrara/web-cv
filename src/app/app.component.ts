04d61aeb32ecc3a19bc589ed7905f9722295c3c5
Daniele Formichelli <daniele.forichelli@gmail.com>
2017-05-06 18:14:30 +0200

    Added support for multiple languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,52 +2,27 @@ import { Component } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './bootstrap.min.css', './short.css', './print.css']
+  styleUrls: ['./app.component.css', './bootstrap.min.css', './print.css']
 })
 export class AppComponent {
-  private jsonFile = 'data.json';
-  personalData: any;
-  education: any;
-  jobs: any;
-  languages: any;
-  itSkills: any;
-  presentations: any;
-  publications: any;
+  private dataDirectory = 'data/';
+  private jsonMainFile = 'main.json';
+  private jsonLabelsFile = 'labels.json';
+  private jsonDataFile = 'data.json';
+  labels: any;
+  data: any;
 
-  constructor(http: Http) {
-    http
-      .get(this.jsonFile)
-      .map((res: Response) => this.readJson(res.json()))
-      .catch(this.handleError)
-      .subscribe();
+  constructor(private http: Http) {
   }
 
-  private readJson(jsonData: any) {
-    this.personalData = jsonData.personalData;
-    this.education = jsonData.education;
-    this.jobs = jsonData.jobs;
-    this.languages = jsonData.languages;
-    this.itSkills = jsonData.itSkills;
-    this.presentations = jsonData.presentations;
-    this.publications = jsonData.publications;
-  }
-
-  private handleError(error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
+  async ngOnInit() {
+    let language = (await this.http.get(this.dataDirectory + this.jsonMainFile).toPromise()).json().language;
+    this.labels = (await this.http.get(this.dataDirectory + language + '/' + this.jsonLabelsFile).toPromise()).json();
+    this.data = (await this.http.get(this.dataDirectory + language + '/' + this.jsonDataFile).toPromise()).json();
   }
 }
